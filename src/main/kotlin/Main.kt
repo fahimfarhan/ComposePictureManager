3cdae97e19f5e943a6444b8be3d764530ac1b028
Soumic <github@gamegame.anonaddy.me>
2024-06-15 23:07:04 +0600

    :rocket: The end

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import ui.App
 
 
 fun main() = application {
-  Window(onCloseRequest = ::exitApplication) {
+  Window(
+    onCloseRequest = ::exitApplication,
+    state = rememberWindowState(size = DpSize(1600.dp, 800.dp))
+  ) {
     val mApp = App()
     mApp.appHome()
   }
